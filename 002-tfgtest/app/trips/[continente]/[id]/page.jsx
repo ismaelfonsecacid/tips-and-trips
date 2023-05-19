cf228615538fe28d548fe70fdcc5da230eb4f099
Ismael <isma@isma.es>
2023-05-19 08:56:52 +0200

    Nuevo json

---
+++ 002-tfgtest/app/trips/[continente]/[id]/page.jsx
@@ -0,0 +1,16 @@
+
+import jsonData from '../../../json/posts.json' 
+
+export default function TripPage ({params}) {
+
+    const { id } = params
+
+    const datosFiltrados = jsonData.filter(item => item.id === id);
+    return (
+        <div>
+            {datosFiltrados.map(item=> (
+                <h2 key={item.id}>{item.body}</h2>
+            ))}
+        </div>
+    )
+}
