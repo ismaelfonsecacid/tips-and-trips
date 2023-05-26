7bc4814ce760711243f13ca20486dfe8965e027a
Ismael <isma@isma.es>
2023-05-26 10:40:18 +0200

    Reload Page

--- 002-tfgtest/app/not-found.jsx
+++ 002-tfgtest/app/not-found.jsx
@@ -1,28 +1,21 @@
-'use client'
 
-import { useEffect } from 'react';
+
 import Link from 'next/link';
-import styles from './page.module.css' 
+import Reload from './components/Reload';
 
-import Loading from 'react-loading';
+
 
 const NotFound = () => {
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      window.location.href = '/';
-    }, 5000);
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
 
   return (
     <div>
       <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
         <h1>404 - Página no encontrada</h1>
         <p>Lo sentimos, la página que estás buscando no existe.</p>
-        <Loading type="bubbles" color="#000" height={30} width={30} />
+        <br />
+        <p>Serás redirigido a la página principal lo antes posible</p>
+        <Reload/>
         <Link href="/">
           Volver a la página de inicio
         </Link>
