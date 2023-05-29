e7bf9f2a4b2e2ad860879f839172847e8170ca87
Ismael <isma@isma.es>
2023-05-29 16:30:07 +0200

    key

--- 002-tfgtest/app/components/Carousel.jsx
+++ 002-tfgtest/app/components/Carousel.jsx
@@ -13,7 +13,7 @@ const Slider = () => {
   const data = jsonData.map((item) => {
     return (
 
-      <div className={styles.card} draggable>
+      <div className={styles.card} key={item.id}>
         <Link href={`/trips/${item.continente}/${item.id}`} key={item.id} style={{ cursor: 'grab', textDecoration: 'none' }}>
           <Image src={item.img} alt={item.ciudad} width={'200'} height={'300'} />
           <p>{item.resume}</p>
