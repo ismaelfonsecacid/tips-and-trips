4bbaca2ad4cb0c78de83a6f00271a60d1e090771
Ismael <isma@isma.es>
2023-06-15 22:36:11 +0200

    reload de una api cada 10s

--- 002-tfgtest/app/components/Slider.jsx
+++ 002-tfgtest/app/components/Slider.jsx
@@ -1,38 +1,46 @@
+'use client'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Slider.module.css';
+import { fetchData } from '../services/apiFetchData';
+import useSWR from 'swr';
 
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from './Slider.module.css'
-import { fetchData } from '../services/apiFetchData'
+const fetchSliderData = async (URL) => {
+  const response = await fetchData(URL);
+  return response;
+};
 
+const Slider = () => {
+  const URL = 'dataResultsPrueba';
+  const { data: sliderData, mutate } = useSWR(URL, fetchSliderData);
 
+  const renderSliderCards = () => {
+    if (!sliderData) {
+      return <p>Cargando datos...</p>;
+    }
 
-export default async function Slider() {
-  const URL = 'dataResultsPrueba'
-  let data = await fetchData(URL);
-  let info = data.map((item) => {
-    return (
-
+    return sliderData.map((item) => (
       <div className={styles.card} key={item.id}>
         <Link href={`/trips/${item.continente}/${item.id}`} key={item.id} style={{ cursor: 'grab', textDecoration: 'none' }}>
           <Image src={item.img} alt={item.ciudad} width={'200'} height={'300'} />
           <p>{item.resume}</p>
         </Link>
       </div>
+    ));
+  };
 
+  // Set the automatic revalidation every 10 seconds (10000 ms)
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      mutate();
+      console.log('mutate')
+    }, 10000);
 
-    );
-  });
+    return () => clearInterval(interval);
+  }, [mutate]);
 
+  return <main className={styles.carrusel}>{renderSliderCards()}</main>;
+};
 
-  return (
-
-    <main className={styles.carrusel}>
-
-      {info}
-
-    </main>
-
-  )
-}
-
+export default Slider;
