2062e13e3c9ea7896323fd77619052cb9f3c0099
Ismael Fonseca Cid <98956154+ismaelfonsecacid@users.noreply.github.com>
2024-01-09 13:43:14 +0100

    Delete 05-server-side-auth directory

--- 05-server-side-auth/app/api/login.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export default function LoginPage(req: NextApiRequest, res : NextApiResponse) {
-    console.log(req.body)
-    res.json({num:Math.floor(Math.random()*10)})
-}
