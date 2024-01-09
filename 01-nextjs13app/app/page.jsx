724732e37db8e2ca574c2ab630424443447fbbc0
Ismael Fonseca Cid <98956154+ismaelfonsecacid@users.noreply.github.com>
2024-01-09 13:42:34 +0100

    Delete 01-nextjs13app directory

--- 01-nextjs13app/app/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function HomePage() {
-    return (
-        <h1>Esta es nuestra app</h1>
-    )
-}
--- 01-nextjs13app/app/posts/[id]/comments/loading.jsx
+++
@@ -1,6 +0,0 @@
-export default function Loading () {
-
-    return (
-        <p>Cargando comentarios</p>
-    )
-}
